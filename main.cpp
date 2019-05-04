845194b313dc558599fbe7f2893e1e03455a3cc1
zgigorigor <49338720+zgigorigor@users.noreply.github.com>
2019-05-04 21:39:50 +0200

    zadatak

---
+++ main.cpp
@@ -0,0 +1,6 @@
+/*Napraviti pojednostavljenu simulaciju kartaske igre blackjack. Korisnika se pita hoce li uzeti kartu, 
+y znaci da, n znaci ne. Ukoliko se korisnik odluci za kartu, dodaje mu se ta karta. Cilj je skupiti zbroj 21 
+ili sto blize tom broju. Ukoliko korisnik ima zbroj veci od 21, automatski gubi igru. Nakon korisnika racunalo bira karte, 
+racunalo ce odustati tj. nece vise uzimati karte ukoliko ili ima bolji zbroj od igraca ili je u rasponu 13 - 21.*/
+
+
